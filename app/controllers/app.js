b41b9fa739b307491ddb02a87ee093cdb4dc3c92
lizhaobomb <lizhaobomb@163.com>
2017-02-16 18:23:30 +0800

    添加短信验证
    添加短信验证

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -1,7 +1,55 @@
 'use strict'
 
+var mongoose = require('mongoose')
+var session = require('koa-session')
+var User = mongoose.model('User')
+
 exports.signature = function *(next) {
 	this.body = {
 		success: true
 	}
 }
+
+exports.hasBody = function *(next) {
+	var body = this.request.body || {}
+	if (Object.keys(body).length === 0) {
+		this.body = {
+			success: false,
+			err: '是不是漏掉了什么了'
+		}
+		return next
+	}
+	yield next
+}
+
+exports.hasToken = function *(next) {
+	var accessToken = this.query.accessToken
+	if (!accessToken) {
+		var accessToken = this.request.body.accessToken
+	}
+
+	if (!accessToken) {
+		this.body = {
+			success: false,
+			err: '钥匙丢了'
+		}
+		return next
+	}
+
+	var user = yield User.findOne({
+		accessToken: accessToken
+	}).exec()
+
+	if (!user) {
+		this.body = {
+			success: false,
+			err: '用户未登录'
+		}
+		return next
+	}
+
+	this.session = this.session || {}
+	this.session.user = user
+	yield next
+}
+
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,21 +1,30 @@
 'use strict'
 
 var mongoose = require('mongoose')
+var session = require('koa-session')
 var xss = require('xss')
+var uuid = require('uuid')
+var sms = require('../services/sms')
 
 var User = mongoose.model('User')
 
 exports.signup = function *(next) {
-	// var phoneNumber = this.request.body.phoneNumber
-	var phoneNumber = this.query.phoneNumber
+	var phoneNumber = xss(this.request.body.phoneNumber.trim())
 	
 	var user = yield User.findOne({
 		phoneNumber: phoneNumber
 	}).exec()
 
+	var verifyCode = sms.getCode()
+
 	if (!user) {
+		var accessToken = uuid.v4()
 		user = new User({
-			phoneNumber: xss(phoneNumber)
+			phoneNumber: xss(phoneNumber),
+			verifyCode: verifyCode,
+			accessToken: accessToken,
+			nickname: '哈哈哈',
+			avatar: 'http://7xibfi.com1.z0.glb.clouddn.com/user_avatar/bbs.iosre.com/lanvsblue/120/2807_1.png'
 		})
 	} else {
 		user.verifyCode = '1212'
@@ -28,7 +37,21 @@ exports.signup = function *(next) {
 		this.body = {
 			success: false
 		}
-		return
+		return next
+	}
+
+	var msg = '您的注册验证码是：' + verifyCode
+	try {
+
+		sms.send(user.phoneNumber, msg)
+	}
+	catch (e) {
+		console.log(e)
+		this.body = {
+			success: false,
+			err: '短信服务异常'
+		}
+		return next
 	}
 
 	this.body = {
@@ -37,14 +60,68 @@ exports.signup = function *(next) {
 }
 
 exports.verify = function *(next) {
-	this.body = {
-		success: true
+	var verifyCode = this.request.body.verifyCode
+	var phoneNumber = this.request.body.phoneNumber
+	if (!verifyCode || !phoneNumber) {
+		this.body = {
+			success: false,
+			err: '验证没通过'
+		}
+		return next
+	}
+
+	var user = yield User.findOne({
+		phoneNumber: phoneNumber,
+		verifyCode: verifyCode
+	}).exec()
+
+	if (user) {
+		user.verified = true
+		user = yield user.save()
+
+		this.body = {
+			success: true,
+			data: {
+				nickname: user.nickname,
+				accessToken: user.accessToken,
+				avatar: user.avatar,
+				_id: user._id
+			}
+		}
+
+	} else {
+		this.body = {
+			success: false,
+			err: '验证未通过'
+		}
 	}
 }
 
 exports.update = function *(next) {
+
+	var body = this.request.body
+	var user = this.session.user
+	var fields = 'avatar,gender,age,nickname,breed'.split(',')
+
+	fields.forEach(function(field) {
+		if (body[field]) {
+			user[field] = xss(body[field].trim())
+		}
+	})
+
+	user = yield user.save()
+
 	this.body = {
-		success: true
+		success: true,
+		data: {
+			nickname: user.nickname,
+			accessToken: user.accessToken,
+			avatar: user.avatar,
+			age: user.age,
+			breed: user.breed,
+			gender: user.gender,
+			_id: user._id
+		}
 	}
 }
 
